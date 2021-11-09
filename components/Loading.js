66d11fd78590cd62d98b048f8cdd429825207c19
IT-Edris <adrismalya@gmail.com>
2021-11-09 12:52:03 +0430

    Invalid attributes deleted

--- components/Loading.js
+++ components/Loading.js
@@ -2,7 +2,7 @@ const Loading = ({size=20,}) => {
     return (
         <div>
             <svg className="spinner" width={`${size}px`} height={`${size}px`} viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-                <circle className="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+                <circle className="path" fill="none" cx="33" cy="33" r="30" />
             </svg>
         </div>
     );
